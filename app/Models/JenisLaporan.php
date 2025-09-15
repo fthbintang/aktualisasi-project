6bf425c1f03bbe85d5a00cac7907d2043e49c3a5
fthbintang <bintangfathehah@gmail.com>
2025-09-16 08:57:21 +0900

    refactor: Delete all soft delete (again)

--- app/Models/JenisLaporan.php
+++ app/Models/JenisLaporan.php
@@ -11,7 +11,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class JenisLaporan extends Model
 {
     use HasFactory;
-    use SoftDeletes;
 
     protected $table = 'jenis_laporan';
     protected $guarded = ['id'];
